1e50c2e2db5681f66e67103612a13c074625257c
Ak-Shaw <ayushshawz@gmail.com>
2020-09-10 05:08:40 +0530

    added home background

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className = 'home'>
+            <div className = 'home__container'>
+
+                <img className = 'home__image' src = 'images/amazon-prime-tv-shows-promo.webp' />
+
+            </div>
+
+            <div className = "home__row">
+                {/* Product */}
+                {/* Product */}
+            </div>
+
+            <div className = "home__row">
+                {/* Product */}
+                {/* Product */}
+                {/* Product */}
+            </div>
+
+            <div className = "home__row">
+                {/* Product */}
+            </div>
+        </div>
+    )
+}
+
+export default Home
