9260a8460e0143c8d8b4575557e79d6c24ded531
Ak-Shaw <ayushshawz@gmail.com>
2020-09-10 16:27:08 +0530

    installed react-router-dom and implemented Switch for default home and checkout pages using Route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          
+          <Route path = "/checkout">
+            <Header />
+            
+          </Route>
+
+          <Route path = "/">
+            <Header />
+            <Home />
+          </Route>
+
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
 
