544c273baf4ea20bfbc753d6d67132ce5e2086db
Ak-Shaw <ayushshawz@gmail.com>
2020-09-10 14:15:37 +0530

    added product component

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import './Home.css';
+import Product from './Product';
 
 function Home() {
     return (
         <div className = 'home'>
             <div className = 'home__container'>
 
-                <img className = 'home__image' src = 'images/amazon-prime-tv-shows-promo.webp' />
+                <img className = 'home__image' src = 'images/amazon-home-page.jpg' />
 
             </div>
 
             <div className = "home__row">
-                {/* Product */}
-                {/* Product */}
+                <Product 
+                    title = 'The Lean Startup'
+                    price = {29.99}
+                    image = 'images/the-lean-startup.jpg'
+                    rating = {5}
+                />
+                <Product />
+                
             </div>
 
             <div className = "home__row">
-                {/* Product */}
-                {/* Product */}
-                {/* Product */}
+                <Product />
+                <Product />
+                <Product />
             </div>
 
             <div className = "home__row">
-                {/* Product */}
+                <Product />
             </div>
         </div>
     )
